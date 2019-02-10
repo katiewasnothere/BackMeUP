7a1a3ac3c7c93bba0824dd3a9ffdf0fcca36b7af
Kathryn <KathrynBaldauf@utexas.edu>
2019-02-09 20:07:26 -0600

    final version for judging

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # BackMeUP
 A raspberry pi program to automatically copy photos from an SD card to a separate USB device
+
+
+# Setup on Raspberry Pi
+
+## Clone from our repo
+
+## Update rc.local to run on boot
+
+https://www.raspberrypi.org/documentation/linux/usage/rc-local.md
+
+
+# Use
+
+## Plug in Raspberry Pi to turn on
+
+## Insert Src USB first
+
+## Insert Dest USD second
+
+## Wait until you hear music to confirm completion
--- src/run_backup.sh
+++ src/run_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-disks=`df -h  | tr -s ' ' | cut -d" " -f 9 | grep "/Volumes/"`
+disks=`df -h  | tr -s ' ' | cut -d" " -f 6 | grep "/media/pi/"`
 disks_arr=($disks)
 original_length=${#disks_arr[@]}
 
@@ -12,16 +12,26 @@ new_arr=()
 new_arr_length=0
 
 while true; do
-	new_disks=`df -h  | tr -s ' ' | cut -d" " -f 9 | grep "/Volumes/"`
+	new_disks=`df -h  | tr -s ' ' | cut -d" " -f 6 | grep "/media/pi/"`
 	new_arr=($new_disks)
 	new_arr_length=${#new_arr[@]}
-	echo "new len: $new_arr_length"
 	temp_length=$((original_length + 2))
 	if [[ $new_arr_length == $temp_length ]]; then
 		disk1=${new_arr[$((original_length + 0))]}
 		disk2=${new_arr[$((original_length + 1))]} 
+		echo "got both disks"
+		echo $disk1
+		echo $disk2
 		break
 	fi
 done
 
-rsync -a -P $disk1 $disk2
+rsync -a -P $disk1/ $disk2
+
+if [ "$?" -eq "0" ]; then
+	# success
+	omxplayer --no-keys ~/BackMeUp/music/centuryfox.mp3 &
+else
+	# some sort of failure
+	omxplayer --no-keys ~/BackMeUp/music/womp.mp3 &
+fi
